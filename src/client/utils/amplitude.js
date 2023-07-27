b5911a5ade12321003c3141c2c54498c5b3e482c
jinyu112 <jinyuguan1102@gmail.com>
2023-07-27 19:53:05 -0400

    add amp file

---
+++ src/client/utils/amplitude.js
@@ -0,0 +1,53 @@
+import amplitude from 'amplitude-js';
+import CONSTANTS from './constants';
+import { determineUserFunctionalityFromUserDoc } from './misc';
+
+export const initAmplitude = () => {
+    amplitude.getInstance().init(process.env.REACT_APP_AMPLITUDE_API_KEY);
+};
+
+export const setAmplitudeUserId = email => {
+    amplitude.getInstance().setUserId(email);
+};
+
+export const sendAmplitudeData = (eventType, eventProperties) => {
+    amplitude.getInstance().logEvent(eventType, eventProperties);
+};
+
+export const setAmplitudeUserData = userProperties => {
+    amplitude.getInstance().setUserProperties(userProperties)
+};
+
+export const amplitudeDataHandler = async (userEmail = false, amplitudeData, userHasPaid) => {
+    try {
+        initAmplitude();
+        if (userEmail !== false && userEmail) setAmplitudeUserId(userEmail);
+        setAmplitudeUserData({ 'addOnPurchased': userHasPaid });
+        sendAmplitudeData(amplitudeData.eventName, amplitudeData.eventProperties);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const sendToAmplitude = (eventName, eventProperties = null, user) => {
+    // Send to Amplitude
+    try {
+        const functionalityTier = determineUserFunctionalityFromUserDoc(user);
+        const {
+            FULL_FUNC,
+            FULL_FUNC_SUB,
+        } = CONSTANTS.FUNC_TIERS;
+
+        const userHasPaid = functionalityTier === FULL_FUNC || functionalityTier === FULL_FUNC_SUB;
+        let dataToAmp = {
+            eventName,
+            eventProperties: null,
+        };
+        if (eventProperties) {
+            dataToAmp.eventProperties = eventProperties;
+        }
+
+        amplitudeDataHandler(user.email, dataToAmp, userHasPaid);
+    }
+    catch (e) { }
+}
